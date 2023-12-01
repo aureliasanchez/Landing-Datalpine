cd22802bc339f291448f6e0b83479b7d2edad673
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-12-01 09:08:46 -0600

    files

---
+++ build/precache-manifest.d385a2864368f2afcde62456ea6ee2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52e1547a28de27b6381",
+    "url": "/static/css/main.266afd20.chunk.css"
+  },
+  {
+    "revision": "e52e1547a28de27b6381",
+    "url": "/static/js/main.e52e1547.chunk.js"
+  },
+  {
+    "revision": "ac05f885bb804ccffeaa",
+    "url": "/static/css/1.bd2d6a73.chunk.css"
+  },
+  {
+    "revision": "ac05f885bb804ccffeaa",
+    "url": "/static/js/1.ac05f885.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9af6dff8071b4d1db033e11895587ee0",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.e7392c150f47e45de440b5b18bdefc03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "608b8362b240d1587be4",
+    "url": "/static/css/main.395b9295.chunk.css"
+  },
+  {
+    "revision": "608b8362b240d1587be4",
+    "url": "/static/js/main.608b8362.chunk.js"
+  },
+  {
+    "revision": "a75f4c92f3120722521a",
+    "url": "/static/css/1.94604f7c.chunk.css"
+  },
+  {
+    "revision": "a75f4c92f3120722521a",
+    "url": "/static/js/1.a75f4c92.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "730b7b08b80adba93a118cf5638951dd",
+    "url": "/index.html"
+  }
+];
